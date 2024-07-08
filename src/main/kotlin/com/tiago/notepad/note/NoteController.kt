b93f6e6e5b5457e1a5cd7839cba8691d2022528c
Tiago Viana <tiago.viana@sonda.com>
2024-07-08 12:17:30 -0300

    Modificada anotação de controller para restController para configurar corretamente o tipo deste controller.

--- src/main/kotlin/com/tiago/notepad/note/NoteController.kt
+++ src/main/kotlin/com/tiago/notepad/note/NoteController.kt
@@ -1,17 +1,15 @@
 package com.tiago.notepad.note
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
 
-@Controller
+@RestController
 @RequestMapping("notes")
 class NoteController {
 
     @GetMapping
-    @ResponseBody
     fun list(): List<Note> {
         return listOf(
             Note("Leitura", "Livro de Spring Boot"),

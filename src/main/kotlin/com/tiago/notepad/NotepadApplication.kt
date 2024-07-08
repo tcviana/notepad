b2c6dd3e6cce50b95bc77701f0f457d35a775ab5
Tiago Viana <tiago.viana@sonda.com>
2024-07-08 11:29:59 -0300

    Add spring boot project.

---
+++ src/main/kotlin/com/tiago/notepad/NotepadApplication.kt
@@ -0,0 +1,11 @@
+package com.tiago.notepad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotepadApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotepadApplication>(*args)
+}
---
+++ src/test/kotlin/com/tiago/notepad/NotepadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tiago.notepad
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotepadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

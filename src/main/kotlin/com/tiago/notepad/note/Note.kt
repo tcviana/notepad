bf98f778d5fd83f8d2d444934f6675d8ad08608a
Tiago Viana <tcviana.sp@gmail.com>
2024-07-09 18:53:30 -0300

    Adicionada anotações do JPA na classe Note.

--- src/main/kotlin/com/tiago/notepad/note/Note.kt
+++ src/main/kotlin/com/tiago/notepad/note/Note.kt
@@ -1,3 +1,14 @@
 package com.tiago.notepad.note
 
-class Note(val title: String = "", val description: String = "")
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Note(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long? = null,
+    val title: String = "",
+    val description: String = ""
+)

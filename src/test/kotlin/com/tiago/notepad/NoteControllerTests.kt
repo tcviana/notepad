57a8ad9a1750f114efc529df7fa100f895a31ad7
Tiago Viana <tcviana.sp@gmail.com>
2024-07-15 10:42:26 -0300

    Adicionado teste para uma nota existente.

--- src/test/kotlin/com/tiago/notepad/NoteControllerTests.kt
+++ src/test/kotlin/com/tiago/notepad/NoteControllerTests.kt
@@ -5,14 +5,13 @@ import com.tiago.notepad.note.Note
 import com.tiago.notepad.note.NoteController
 import com.tiago.notepad.note.NoteRepository
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.`when`
+import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(NoteController::class)
@@ -55,6 +54,18 @@ class NoteControllerTests @Autowired constructor(
             .andExpect(jsonPath("$.id").value(1))
             .andExpect(jsonPath("$.title").value(title))
             .andExpect(jsonPath("$.description").value(description))
+    }
+
+    @Test
+    fun `should delete a note`() {
+        val noteId = 1L
+        doNothing().`when`(noteRepository).deleteById(noteId)
+
+        mockMvc.perform(
+            delete("/notes/{id}", noteId)
+        ).andExpect(status().isNoContent)
+
+        verify(noteRepository, times(1)).deleteById(noteId)
     }
 
     private fun retrieveCreatedNote(): Note = Note(1, title, description)

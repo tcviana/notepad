56b6883776945c24f2f477c1513df713b7b0b62b
Tiago Viana <tcviana.sp@gmail.com>
2024-07-19 13:28:59 -0300

    Adicionada classe NoteDTO

---
+++ src/main/kotlin/com/tiago/notepad/note/NoteDTO.kt
@@ -0,0 +1,18 @@
+package com.tiago.notepad.note
+
+/**
+ * A classe [NoteDTO] é responsável pelo tratamento dos campos da entidade [Note]
+ *
+ * @property id Identificador único da nota.
+ * @property title Título da nota.
+ * @property description Descrição da nota.
+ *
+ * @author tcviana
+ * @since 2024-07-19
+ */
+
+data class NoteDTO(
+    val id: Long? = null,
+    val title: String,
+    val description: String
+)

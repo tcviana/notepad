61d8809653cb453732cbfc60f14b88c4a569ae69
Tiago Viana <tcviana.sp@gmail.com>
2024-07-19 14:00:16 -0300

    Adicionado DTO no Controller.

--- src/main/kotlin/com/tiago/notepad/note/NoteMapper.kt
+++ src/main/kotlin/com/tiago/notepad/note/NoteMapper.kt
@@ -3,6 +3,9 @@ package com.tiago.notepad.note
 /**
  * A classe [NoteMapper] é um singleton responsável pela conversão de um objeto
  * do tipo [Note] para [NoteDTO] e vice-versa
+ *
+ * @author tcviana
+ * @since 2024-07-19
  */
 object NoteMapper {
     fun toEntity(noteDTO: NoteDTO): Note {

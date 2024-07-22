8220f2f7f8d38b3ece7b720e6fc0bc41d3e7166e
Tiago Viana <tcviana.sp@gmail.com>
2024-07-21 23:20:09 -0300

    Corrigida configuração do redis.

--- src/main/kotlin/com/tiago/notepad/infrastructure/CacheConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.tiago.notepad.infrastructure
-
-import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@EnableCaching
-class CacheConfig
--- src/main/kotlin/com/tiago/notepad/infrastructure/RedisConfig.kt
+++ src/main/kotlin/com/tiago/notepad/infrastructure/RedisConfig.kt
@@ -1,13 +1,32 @@
 package com.tiago.notepad.infrastructure
 
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
+@EnableCaching
 class RedisConfig {
+
+    @Bean
+    fun cacheManager(redisConnectionFactory: RedisConnectionFactory): RedisCacheManager {
+        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+            )
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(redisCacheConfiguration)
+            .build()
+    }
 
     @Bean
     fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {

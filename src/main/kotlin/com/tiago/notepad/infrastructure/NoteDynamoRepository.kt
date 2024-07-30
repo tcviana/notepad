117ce5e655b9cf6cb54a4d3947b0b77f9d16cd9b
Tiago Viana <tcviana.sp@gmail.com>
2024-07-29 23:30:04 -0300

    Adicionado repository para o H2 e DynamoDB

---
+++ src/main/kotlin/com/tiago/notepad/infrastructure/NoteDynamoRepository.kt
@@ -0,0 +1,22 @@
+package com.tiago.notepad.infrastructure
+
+import com.tiago.notepad.domain.note.Note
+import com.tiago.notepad.domain.note.NoteRepository
+import org.springframework.context.annotation.Profile
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
+
+/**
+ * Repositório para a entidade [Note].
+ *
+ * Este repositório fornece métodos CRUD padrão para a entidade [Note],
+ * herdando da interface [JpaRepository] e [JpaSpecificationExecutor] para filtros
+ *
+ * @author tcviana
+ * @since 2024-07-29
+ */
+@Repository
+@Profile("docker")
+interface NoteDynamoRepository : NoteRepository {
+}
